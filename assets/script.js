d4e821746c8ae0dad5e8136fe50a60879508d966
nlenno1 <nick.lennon@hotmail.co.uk>
2021-05-28 17:15:52 +0000

    feat: added hover and click events to Career Highlights section with styling

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,30 @@
+
 console.log("js linked")
 
-$("#highlight-1").ho
-
-function fillTimelineElement (elementName, text) {
-
+function highlightHoverEvent (hoverLocationId) {
+    $(`${hoverLocationId} .timeline-item-top`).hover(
+        function() {
+            $(`${hoverLocationId} .timeline-item-expand`).slideDown().css("display", "block");;
+        }, function() {
+            $(`${hoverLocationId} .timeline-item-expand`).slideUp().css("display", "hidden");
+        }
+    );
 }
+
+function clickHighlightEvent (LocationId) {
+    $(`${LocationId} .timeline-item-top`).click(function() {
+        if ($(`${LocationId} .timeline-item-expand`).css("display") == "block") {
+            $(`${LocationId} .timeline-item-expand`).slideUp().css("display", "hidden");
+        }
+    });
+}
+
+
+
+highlightHoverEvent ("#highlight-1")
+highlightHoverEvent ("#highlight-2")
+highlightHoverEvent ("#highlight-3")
+
+clickHighlightEvent ("#highlight-1")
+clickHighlightEvent ("#highlight-2")
+clickHighlightEvent ("#highlight-3")
