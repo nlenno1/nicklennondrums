8c6aed88b19ac75e047c46064111df8952013748
nlenno1 <nick.lennon@hotmail.co.uk>
2021-06-10 23:07:04 +0000

    feat: hover popovers added to services icons

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,15 @@
-console.log("js linked")
+$(window).ready (function() {
+    console.log("js linked");
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+    })
+
+    highlightHoverEvent ("#highlight-1");
+    highlightHoverEvent ("#highlight-2");
+    highlightHoverEvent ("#highlight-3");
+});
 
 function highlightHoverEvent (hoverLocationId) {
     $(`${hoverLocationId} .timeline-item-top`).hover(
@@ -9,7 +20,3 @@ function highlightHoverEvent (hoverLocationId) {
         }
     );
 }
-
-highlightHoverEvent ("#highlight-1")
-highlightHoverEvent ("#highlight-2")
-highlightHoverEvent ("#highlight-3")
